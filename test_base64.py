454ad71435eda3f7b318c3e9b4cca08ebc3866c3
ReedSun <sunhongzhao@foxmail.com>
2016-10-08 21:24:20 +0800

    add base64 test

---
+++ test_base64.py
@@ -0,0 +1,13 @@
+#写一个能处理去掉=的base64解码函数
+
+# -*- coding: utf-8 -*-
+
+import base64
+
+def safe_base64_decode(s):
+
+    return base64.b64decode(s+b"="*(4-len(s)%4))
+# 测试:
+assert b'abcd' == safe_base64_decode(b'YWJjZA=='), safe_base64_decode('YWJjZA==')
+assert b'abcd' == safe_base64_decode(b'YWJjZA'), safe_base64_decode('YWJjZA')
+print('Pass')
